046c21d1dbbbc059a89d6c122eee226705f4ae7e
Chris Wallerstein <t@cmgwallerstein.net>
2022-01-13 16:42:02 -0500

    refactor: Refactor etl.py and dl.cfg
    - Split out long functions into shorter units
    - Removed dependency on configparser and configuration file as it is unnecessary. AWS credentials come from environment variables now.
    - Delete `dl.cfg`

--- etl.py
+++ etl.py
@@ -1,17 +1,13 @@
-import configparser
-from datetime import datetime
 import os
+
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import udf, col, from_unixtime
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dayofweek, to_timestamp, to_utc_timestamp
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dayofweek, to_timestamp
 from pyspark.sql.types import StructType, StructField as Fld, DoubleType as Dbl, StringType as Str, \
-    IntegerType as Int, LongType as Lng, TimestampType as TStamp
+    IntegerType as Int, LongType as Lng
 
-config = configparser.ConfigParser()
-config.read('dl.cfg')
-
-os.environ['AWS_ACCESS_KEY_ID'] = config.get('CREDENTIALS', 'AWS_ACCESS_KEY_ID')
-os.environ['AWS_SECRET_ACCESS_KEY'] = config.get('CREDENTIALS', 'AWS_SECRET_ACCESS_KEY')
+"""
+Functions for extracting records from json files in s3 and populating cleaned and transformed tables 
+"""
 
 
 def create_spark_session() -> SparkSession:
@@ -79,7 +75,96 @@ def process_log_data(spark: SparkSession, songs_table: DataFrame) -> (DataFrame,
     Load log file from s3 into staging table then populate users, time and song_play tables
     :param spark: SparkSession object
     :param songs_table: DataFrame containing songs records
-    :return: None
+    :return: Tuple containing users, time and songplays tables
+    """
+    log_data_staging = populate_log_staging_table(spark)
+    users_table = populate_user_table(log_data_staging)
+    log_data_staging, time_table = populate_time_table(log_data_staging, spark)
+    songplays_table = populate_songplays_table(log_data_staging, songs_table, time_table)
+    return users_table, time_table, songplays_table
+
+
+def populate_songplays_table(log_data_staging: DataFrame, songs_table: DataFrame, time_table: DataFrame) -> DataFrame:
+    """
+    Populate songplays time from join of staging and songs tables
+    Persist to s3 partitioned by year and month
+    :param log_data_staging: Staging table
+    :param songs_table: Songs table
+    :param time_table: Time table
+    :return: songplays table
+    """
+    join_table = log_data_staging.join(
+        songs_table, (log_data_staging.song == songs_table.title) & (log_data_staging.length == songs_table.duration),
+        how="left"
+    )
+    songplays_table = join_table.select(
+        join_table.ts.alias("start_time"),
+        join_table.userId.cast("int").alias("user_id"),
+        join_table.level,
+        join_table.song_id,
+        join_table.artist_id,
+        join_table.sessionId.alias("session_id"),
+        join_table.location,
+        join_table.userAgent.alias("user_agent")
+    ).dropDuplicates()
+    # write to s3 partitioned by year and month
+    songplays_table.join(time_table, on=["start_time"], how="inner").write.mode("overwrite") \
+        .partitionBy("year", "month").parquet("s3a://wallerstein-udacity/data_lake/song_plays")
+    return songplays_table
+
+
+def populate_time_table(spark: SparkSession, log_data_staging: DataFrame) -> DataFrame:
+    """
+    Generate correct time and date fields, populate time table
+    Write to s3 and return
+    :param spark: SparkSession
+    :param log_data_staging: Log staging table
+    :return: Populated time table
+    """
+
+    log_data_staging = log_data_staging.withColumn("ts1", log_data_staging.ts / 1000)
+    log_data_staging = log_data_staging.withColumn("timestamp", to_timestamp(log_data_staging.ts1))
+    # extract columns to create time table, retaining UTC time zone
+    spark.conf.set("spark.sql.session.timeZone", "UTC")
+    time_table = log_data_staging.select(
+        log_data_staging.ts.alias("start_time"),
+        hour(log_data_staging.timestamp).alias("hour"),
+        dayofmonth(log_data_staging.timestamp).alias("day"),
+        weekofyear(log_data_staging.timestamp).alias("week"),
+        month(log_data_staging.timestamp).alias("month"),
+        year(log_data_staging.timestamp).alias("year"),
+        dayofweek(log_data_staging.timestamp).alias("weekday"),
+    ).dropDuplicates()
+    spark.conf.unset("spark.sql.session.timeZone")
+    # write to s3
+    time_table.write.mode("overwrite").partitionBy("year", "month") \
+        .parquet("s3a://wallerstein-udacity/data_lake/time_table")
+    return time_table
+
+
+def populate_user_table(log_data_staging: DataFrame) -> DataFrame:
+    """
+    Create user table with field names matching the rest of the schema,
+    populate from staging and write to s3 in parquet format
+    :param log_data_staging: Log staging table
+    :return: Populated user table
+    """
+    users_table = log_data_staging.select(
+        log_data_staging.userId.cast("int").alias("user_id"),
+        log_data_staging.firstName.alias("first_name"),
+        log_data_staging.lastName.alias("last_name"),
+        log_data_staging.gender.alias("gender"),
+        log_data_staging.level.alias("level")
+    ).dropDuplicates()
+    users_table.write.mode("overwrite").parquet("s3a://wallerstein-udacity/data_lake/users_table")
+    return users_table
+
+
+def populate_log_staging_table(spark: SparkSession) -> DataFrame:
+    """
+    Populate log staging table from s3 and return
+    :param spark: SparkSession
+    :return: Populated log staging table
     """
     log_schema = StructType([Fld("artist", Str()),
                              Fld("auth", Str()),
@@ -99,64 +184,9 @@ def process_log_data(spark: SparkSession, songs_table: DataFrame) -> (DataFrame,
                              Fld("ts", Lng()),
                              Fld("userAgent", Str()),
                              Fld("userId", Str())])
-
     log_data_staging = get_json_from_s3(spark, "udacity-dend/log-data/", log_schema).filter("page = 'NextSong'")
-
     print(f"{log_data_staging.count()} songplay log records loaded")
-
-    # create user table with field names matching the rest of the schema
-    users_table = log_data_staging.select(
-        log_data_staging.userId.cast("int").alias("user_id"),
-        log_data_staging.firstName.alias("first_name"),
-        log_data_staging.lastName.alias("last_name"),
-        log_data_staging.gender.alias("gender"),
-        log_data_staging.level.alias("level")
-    ).dropDuplicates()
-
-    users_table.write.mode("overwrite").parquet("s3a://wallerstein-udacity/data_lake/users_table")
-
-    log_data_staging = log_data_staging.withColumn("ts1", log_data_staging.ts / 1000)
-    log_data_staging = log_data_staging.withColumn("timestamp", to_timestamp(log_data_staging.ts1))
-
-    # extract columns to create time table, retaining UTC time zone
-    spark.conf.set("spark.sql.session.timeZone", "UTC")
-    time_table = log_data_staging.select(
-        log_data_staging.ts.alias("start_time"),
-        hour(log_data_staging.timestamp).alias("hour"),
-        dayofmonth(log_data_staging.timestamp).alias("day"),
-        weekofyear(log_data_staging.timestamp).alias("week"),
-        month(log_data_staging.timestamp).alias("month"),
-        year(log_data_staging.timestamp).alias("year"),
-        dayofweek(log_data_staging.timestamp).alias("weekday"),
-    ).dropDuplicates()
-    spark.conf.unset("spark.sql.session.timeZone")
-
-    # write to s3
-    time_table.write.mode("overwrite").partitionBy("year", "month")\
-        .parquet("s3a://wallerstein-udacity/data_lake/time_table")
-
-    # read in song data to use for songplays table
-    join_table = log_data_staging.join(
-        songs_table, (log_data_staging.song == songs_table.title) & (log_data_staging.length == songs_table.duration),
-        how="left"
-    )
-
-    songplays_table = join_table.select(
-        join_table.ts.alias("start_time"),
-        join_table.userId.cast("int").alias("user_id"),
-        join_table.level,
-        join_table.song_id,
-        join_table.artist_id,
-        join_table.sessionId.alias("session_id"),
-        join_table.location,
-        join_table.userAgent.alias("user_agent")
-    ).dropDuplicates()
-
-    # write to s3 partitioned by year and month
-    songplays_table.join(time_table, on=["start_time"], how="inner").write.mode("overwrite")\
-        .partitionBy("year", "month").parquet("s3a://wallerstein-udacity/data_lake/song_plays")
-
-    return users_table, time_table, songplays_table
+    return log_data_staging
 
 
 def main() -> None:
